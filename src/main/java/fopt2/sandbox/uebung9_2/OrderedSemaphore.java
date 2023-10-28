9328cfea1ab62c63ec5116e6b520dd37ffa3b1d9
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-10-28 17:38:13 +0200

    feat: add a linear ordered semaphore as alternative to uebung9_2

---
+++ src/main/java/fopt2/sandbox/uebung9_2/OrderedSemaphore.java
@@ -0,0 +1,55 @@
+package fopt2.sandbox.uebung9_2;
+
+import fopt2.sandbox.Semaphore;
+
+/**
+ * Implementation of an OrderedSemaphore where p() is called with the
+ * preferred order in which a thread requires to access a critical block.
+ *
+ * This implementation does not consider Threads that may be processed parallel, as in Listing 3.3
+ * in the book.
+ *
+ */
+public class OrderedSemaphore extends Semaphore {
+
+    int tickets = 0;
+    int nextTicket = -1;
+
+    public OrderedSemaphore(int length) {
+        super();
+        tickets = length;
+    }
+
+
+    public synchronized void p(int orderRequest) {
+
+        if (nextTicket == -1) {
+            nextTicket = orderRequest;
+        }
+
+
+        while (value == 0 || orderRequest != nextTicket) {
+
+            try {
+                this.wait();
+            } catch (InterruptedException ignored) {
+
+            }
+
+
+        }
+
+        nextTicket = (nextTicket % tickets) + 1;
+        value--;
+        notifyAll();
+    }
+
+
+    public synchronized  void v() {
+
+        value++;
+        notifyAll();
+
+    }
+
+}
---
+++ src/main/java/fopt2/sandbox/uebung9_2/OrderedSemaphoreDemo.java
@@ -0,0 +1,72 @@
+package fopt2.sandbox.uebung9_2;
+
+import fopt2.sandbox.uebung9_1.Lock;
+import org.junit.jupiter.api.Order;
+
+import java.util.Arrays;
+
+class SemaphoreAccessThread extends Thread {
+
+    OrderedSemaphore semaphore;
+
+    int order;
+
+    public SemaphoreAccessThread(OrderedSemaphore semaphore, int order, String name) {
+        this.semaphore = semaphore;
+
+        setName(name);
+
+        this.order = order;
+    }
+
+    public void run() {
+
+        while (true) {
+
+            semaphore.p(this.order);
+
+            System.out.println(Thread.currentThread().getName() + " in Critical section");
+
+           /*try {
+                Thread.sleep((int)(100));
+            } catch (InterruptedException ignores) {
+
+            }*/
+
+            semaphore.v();
+        }
+
+    }
+
+
+}
+
+public class OrderedSemaphoreDemo {
+
+
+
+    public static void main(String[] args) {
+
+        OrderedSemaphore sem = new OrderedSemaphore(3);
+
+        SemaphoreAccessThread[] ths = {
+            new SemaphoreAccessThread(sem, 1, "t1"),
+            new SemaphoreAccessThread(sem, 3, "t2"),
+            new SemaphoreAccessThread(sem, 2, "t3")
+        };
+
+        try {
+            ths[0].start();
+            Thread.sleep(10);
+            ths[1].start();
+            Thread.sleep(10);
+            ths[2].start();
+            Thread.sleep(10);
+
+        } catch (InterruptedException ignored) {
+
+        }
+    }
+
+
+}
