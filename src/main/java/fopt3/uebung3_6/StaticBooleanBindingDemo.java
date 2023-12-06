156c10b213977f2965c1c3b7e8fe3f0412b39956
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-12-06 11:06:30 +0100

    feat: add uebung3_6

---
+++ src/main/java/fopt3/uebung3_6/StaticBooleanBindingDemo.java
@@ -0,0 +1,31 @@
+package fopt3.uebung3_6;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.NumberBinding;
+import javafx.beans.binding.NumberExpression;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class StaticBooleanBindingDemo {
+
+    public static void main(String[] args) {
+
+        SimpleIntegerProperty num1 = new SimpleIntegerProperty();
+        SimpleIntegerProperty num2 = new SimpleIntegerProperty();
+        SimpleIntegerProperty num3 = new SimpleIntegerProperty();
+
+        // num1 > num2 * num3 && num2 > 0
+        BooleanBinding bb1 = Bindings
+            .greaterThan(num1, Bindings.multiply(num2, num3))
+            .and(Bindings.greaterThan(num2, 0));
+
+        assert(!bb1.getValue());
+
+        num1.set(11);
+        num2.set(2);
+        num3.set(5);
+
+        assert(bb1.getValue());
+    }
+
+}
