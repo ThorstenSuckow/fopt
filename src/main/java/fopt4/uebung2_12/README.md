d64e70d8fcc041d99e0e763311b2b41a80458474
ThorstenSuckow <thorsten@suckow-homberg.de>
2023-12-12 16:23:37 +0100

    refactor: add uebung2_14 for fopt4

--- src/main/java/fopt4/uebung2_12/README.md
+++ src/main/java/fopt4/uebung2_12/README.md
@@ -1,4 +1,4 @@
 ## uebung2_12
 
 Der Source Code wurde überwiegend aus dem Lehrmaterial entnommen.
-Anmerkungen zu uebung2_12 sind im Quelltext vermerkt.
+Anmerkungen zu uebung2_12, uebung2_13 und uebung2_14 sind im Quelltext vermerkt.
--- src/main/java/fopt4/uebung2_12/overview/OverviewPresenter.java
+++ src/main/java/fopt4/uebung2_12/overview/OverviewPresenter.java
@@ -51,9 +51,27 @@ public class OverviewPresenter
 
         /**
          * @uebung2_13 set parameter useSingleDialog to false to make sure multiple
-         * dialogs can be shown (will also set Dialog's modality to NONE)
+         * dialogs can be shown (will also set Dialog's modality to NONE).
+         * Using multiple dialogs with showAndWait will also allow to edit one and the same
+         * data at once. However, the ListView will not be refreshed until the last opened dialog was closed (
+         * independent from the order in which closing occurs - if the last dialog gets closed first,
+         * changes are immediately visible, if the first dialog is closed, and 2 remain open, those two have to be
+         * closed first - then the changes from the first opened dialog are reflected):
+         *
+         *   (informal representation)
+         *   open D1 -> event loop E1
+         *   open D2 -> event loop E1 -> event loop E2
+         *   open D3 -> event loop E1 -> event loop E2 -> event loopE3
+         *
+         *   close windows D3 D2 D1 -> changes reflect in the contact in this order - D1 changes will be used for contact
+         *   close windows D2 D3 D1 -> changes reflect when D2 was closed (with changes in D2), however D1 changes will
+         *   make it into the contact.
+         *   Thus, event loops have to be resolved from inner to outer. An inner loop will resolve its outer loop if the
+         *   inner loop does not contain any nested loops anymore.
+         *
+         *
          */
-        boolean useSingleDialog = true;
+        boolean useSingleDialog = false;
         showDetailDialog(contact, useSingleDialog);
     }
 
@@ -79,7 +97,7 @@ public class OverviewPresenter
             detailDialog.getDialogPane().getButtonTypes().setAll(ButtonType.OK, ButtonType.CANCEL);
             /**
              * @uebung2_12 commenting the following line will pass an object of type ButtonType to updateContact.
-             * Since casting is not possible, an exception is thrown.
+             * Since casting is not possible, a ClassCastException is thrown.
              * Note: The return type must have the same type as the type parameter of the Dialog-class.
              */
             detailDialog.setResultConverter((ButtonType bt) -> makeContact(bt, detailView));
@@ -91,7 +109,8 @@ public class OverviewPresenter
         Optional<Contact> result = detailDialog.showAndWait();
         if(result.isPresent())
         {
-            contactModel.updateContact(result.get());
+            Contact c = result.get();
+            contactModel.updateContact(c);
             search();
         }
     }
