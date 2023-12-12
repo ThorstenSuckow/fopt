7b890e8594d89203057e090d232bf77d608484b5
ThorstenSuckow <thorsten@suckow-homberg.de>
2023-12-12 13:38:06 +0100

    feat: add uebung2_7

--- src/main/java/fopt4/uebung2_7/Main.java
+++ src/main/java/fopt4/uebung2_7/Main.java
@@ -1,18 +1,26 @@
-package fopt4.uebung2_5;
+package fopt4.uebung2_7;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-/**
- * See uebung2_7 for StageStyle.UNDECORATED
- */
 public class Main extends Application {
 
     public void start(Stage stage) {
 
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setHeight(300);
+        stage.setWidth(300);
         VBox root = new VBox();
+        root.setAlignment(Pos.TOP_RIGHT);
+        Button b = new Button("Close");
+        b.setOnAction((e) -> stage.hide());
+        root.getChildren().add(b);
         Scene scene = new Scene(root);
 
         // scene's immediate width/height is 0d
@@ -24,6 +32,8 @@ public class Main extends Application {
         stage.show();
 
         // height / width automatically set by application.
+        // since stage is UNDECORATED, the stage's dimensions equal to the scene's dimension
+        // (no title bar taken into account)
         System.out.println("[scene] After show: " + scene.getWidth() + " " + scene.getHeight());
         System.out.println("[stage] After show: " + stage.getWidth() + " " + stage.getHeight());
     }
