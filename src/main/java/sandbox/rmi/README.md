2fc3790478a15c29bebfa06792cd28a716be5304
ThorstenSuckow <thorsten@suckow-homberg.de>
2024-01-02 16:15:55 +0100

    feat: the rmiregistry is started by the RMIIncrementorServer

--- src/main/java/sandbox/rmi/README.md
+++ src/main/java/sandbox/rmi/README.md
@@ -1,6 +1,8 @@
 ## rmi
 
-Start `rmiregistry` with 
+### Usage 
+
+`RMIIncrementorServer` starts the Registry. If the registry should start as an external process, uncomment the appropriate lines in `RMIIncrementorServer` and start the `rmiregistry` with 
 
 ```bash
 $ rmiregistry -J-Djava.class.path=[DIR]
--- src/main/java/sandbox/rmi/RMIIncrementorServer.java
+++ src/main/java/sandbox/rmi/RMIIncrementorServer.java
@@ -1,17 +1,24 @@
 package sandbox.rmi;
 
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class RMIIncrementorServer {
 
     public static void main(String[] args) {
         try {
+
+            Registry registry = LocateRegistry.createRegistry(1099);
+
             DefaultRMIIncrementor incrementor = new DefaultRMIIncrementor();
             /**
              * @note: rebind() "replaces the binding for the specified name in this registry with the supplied
              * remote reference. If there is an existing binding for the specified name, it is discarded."
              */
-            Naming.rebind("incrementor", incrementor);
+            registry.rebind("incrementor", incrementor);
+            // use Naming if Registry is started as external process
+            //Naming.rebind("incrementor", incrementor);
         } catch (Exception e) {
             System.out.println("Excpetion while creating incrementor: " + e);
         }
