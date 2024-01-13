f6976ec8d0523dd0b2dbe99c3aa3bc69e1a4dd59
ThorstenSuckow <thorsten@suckow-homberg.de>
2024-01-13 10:46:11 +0100

    feat: add AttributeListener

---
+++ ee/foptweb/src/main/java/fopt6/sandbox/servletContexListener/AttributeListener.java
@@ -0,0 +1,17 @@
+package fopt6.sandbox.servletContexListener;
+
+import fopt6.sandbox.util.Counter;
+import jakarta.servlet.ServletContextAttributeEvent;
+import jakarta.servlet.ServletContextAttributeListener;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class AttributeListener implements ServletContextAttributeListener {
+
+    @Override
+    public void attributeAdded(ServletContextAttributeEvent event) {
+        System.out.println("   Attribute added: " + event.getName());
+    }
+}
