8c39ade52f8cb2a751ab854bd16f84db57c0be1c
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2024-02-16 18:59:56 +0100

    docs: update Lösungsvorschlag for WS13 Aufgabe 3

--- src/main/java/klausurvorbereitung/foptws23/DynamicParallelServerDemo.java
+++ src/main/java/klausurvorbereitung/foptws23/DynamicParallelServerDemo.java
@@ -10,69 +10,9 @@ import java.net.Socket;
 
 public class DynamicParallelServerDemo {
 
-    static class Server extends Thread {
-
-        static class ServerThread extends Thread {
-
-            Socket server;
-            public ServerThread(Socket s) {
-                server = s;
-                this.start();
-            }
-
-            public void run() {
-
-                try (Socket client = server) {
-                    while (true) {
-
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                        String line = reader.readLine();
-
-                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-
-                        writer.write("server answers " + line);
-                        writer.newLine();
-                        writer.flush();
-
-                    }
-                } catch (Exception e) {
-                    System.out.println("[serverthread] " + e);
-                }
-
-            }
-
-        }
-        public Server() {
-            start();
-        }
-
-        public void run() {
-
-            try (ServerSocket server = new ServerSocket(8888)) {
-
-                while (true) {
-
-
-                    ServerThread serverThread = new ServerThread(server.accept());
-                    System.out.println("       SERVER LOOP");
-
-                }
-
-
-            } catch (Exception e) {
-                System.err.println("[server] " + e);
-            }
-
-        }
-
-
-    }
-
-
-
     static class Client extends Thread {
 
-        private String name;
+        private final String name;
         public Client(String name) {
             this.name = name;
             start();
@@ -103,19 +43,64 @@ public class DynamicParallelServerDemo {
         }
 
     }
+    static class Server {
 
+        class ServerThread extends Thread {
+            private Socket client;
+            public ServerThread(Socket sck) {
+                client = sck;
+                start();
+            }
+            public void run() {
+                try (Socket socket = client) {
+                    while (true) {
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+                        String line = reader.readLine();
+
+                        writer.write("Received line: \"" + line + "\"");
+                        writer.newLine();
+                        writer.flush();
+
+                    }
+                } catch (Exception e) {
+                    System.err.println("[serverthread error] " + e);
+                }
+            }
+        }
+
+        public Server() {
+            init();
+        }
+
+        private void init() {
+
+            try (ServerSocket server = new ServerSocket(8888)) {
+
+                while (true) {
+
+                    try {
+                        new ServerThread(server.accept());
+                        System.out.println("[server] Server accepted new connection!");
+                    } catch (Exception e) {
+                        System.err.println("[server error]  " + e);
+                    }
+
+                }
+            } catch (Exception e) {
+                System.err.println("[server error] " + e);
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
         if (args.length == 0) {
-
             Server s = new Server();
-
-
         } else {
-
             Client c = new Client(args[0]);
-
         }
 
 
